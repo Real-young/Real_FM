93cddd37321419a240ca49af39076a1d92acfc3f
ybm <ybm@ybm.local>
2017-07-01 11:01:27 +0800

    init FM

---
+++ Real_FM/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Real_FM
+//
+//  Created by ybm on 2017/7/1.
+//  Copyright © 2017年 ybm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Real_FM/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Real_FM
+//
+//  Created by ybm on 2017/7/1.
+//  Copyright © 2017年 ybm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
